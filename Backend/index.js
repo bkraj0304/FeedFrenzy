f712520c31008eb8040a0880329f658ad38e0ed9
bkraj0304 <raj0304.nit.m@gmail.com>
2024-07-24 14:09:35 +0530

    port Update

--- Backend/index.js
+++ Backend/index.js
@@ -25,9 +25,9 @@ const verifyJWTMiddleWare=require('./middleware/auth');
 // Middleware setup
 app.use(cors());
 app.use(express.json());
-
+const port=process.env.PORT || 3001;
 // Start the server and connect to the database
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log("Server running on port 3001");
     
 });
